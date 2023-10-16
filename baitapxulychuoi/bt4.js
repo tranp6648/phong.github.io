3f7ef8ac21586cd984888e5f8ad1cc5fa9cf49ff
tranp6648 <tranp6648@gmail.com>
2023-10-16 22:22:48 +0700

    update

--- baitapxulychuoi/bt4.js
+++ baitapxulychuoi/bt4.js
@@ -2,6 +2,9 @@
 //Viết một hàm nhận vào một chuỗi và loại bỏ tất cả các khoảng trắng thừa trong chuỗi. Ví dụ: Nếu chuỗi là " Hello     world    ", hàm sẽ trả về "Hello world".
 
 function khoangtrang(str) {
-    return str.trim();
+   if(str.includes(" ")){
+    return str.replace(/\s+/g," ").trim()
+   }
+   return str;
 }
-console.log(khoangtrang("Hello     world    "))
+console.log(khoangtrang("        Hello     world    "))
